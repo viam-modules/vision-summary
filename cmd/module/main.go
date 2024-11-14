a7c9d920b748aaa7c9143abb3dc66e3fafd9e4e7
Alan Davidson <alan@viam.com>
2024-11-14 16:52:34 -0500

    use ModularMain instead of copypasta

--- cmd/module/main.go
+++ cmd/module/main.go
@@ -2,43 +2,17 @@
 package main
 
 import (
-	"context"
-
 	"go.viam.com/rdk/components/sensor"
 	"go.viam.com/rdk/services/vision"
 
-	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/module"
-	"go.viam.com/utils"
+	"go.viam.com/rdk/resource"
 
 	"github.com/viam-modules/vision-summary/countclassifier"
 	"github.com/viam-modules/vision-summary/countsensor"
 )
 
 func main() {
-	utils.ContextualMain(mainWithArgs, module.NewLoggerFromArgs("vision-summary"))
-}
-
-func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) (err error) {
-	myMod, err := module.NewModuleFromArgs(ctx)
-	if err != nil {
-		return err
-	}
-
-	err = myMod.AddModelFromRegistry(ctx, vision.API, countclassifier.Model)
-	if err != nil {
-		return err
-	}
-	err = myMod.AddModelFromRegistry(ctx, sensor.API, countsensor.Model)
-	if err != nil {
-		return err
-	}
-
-	err = myMod.Start(ctx)
-	defer myMod.Close(ctx)
-	if err != nil {
-		return err
-	}
-	<-ctx.Done()
-	return nil
+	module.ModularMain(resource.APIModel{vision.API, countclassifier.Model},
+	                   resource.APIModel{sensor.API, countsensor.Model})
 }
